38407a71c0c28c1f82546ed9ce974b1359ea4b44
Zach Olauson <zmolauson@gmail.com>
2014-04-22 08:46:26 -0500

    update for XE16

--- src/com/zacholauson/glass/date/DateActivity.java
+++ src/com/zacholauson/glass/date/DateActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
-import android.view.KeyEvent;
 import android.widget.TextView;
 
 import com.google.android.glass.app.Card;
@@ -15,16 +14,11 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public class DateActivity extends Activity {
-
-  private Card _card;
-  private View _cardView;
-  private TextView _statusTextView;
+  TextView statusTextView;
 
   private TextToSpeech _speech;
-
   private Context _context = this;
-
-  private String _date_string = todaysDate();
+  private String date_string = todaysDate();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -33,41 +27,20 @@ public class DateActivity extends Activity {
     _speech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
       @Override
       public void onInit(int status) {
-        _speech.speak(_date_string, TextToSpeech.QUEUE_FLUSH, null);
+        _speech.speak(date_string, TextToSpeech.QUEUE_FLUSH, null);
       }
     });
 
-    _card = new Card(_context);
-    _card.setText(_date_string);
-    _cardView = _card.toView();
-    setContentView(_cardView);
+    Card card = new Card(_context);
+    card.setText(date_string);
+    View view = card.getView();
+    setContentView(view);
 
-    //setContentView(R.layout.layout_date); // use xml layout
-    _statusTextView = (TextView)findViewById(R.id.status);
-  }
-
-  @Override
-  public boolean onKeyDown(int keyCode, KeyEvent event) {
-    return super.onKeyDown(keyCode, event);
-  }
-
-  @Override
-  public void onResume() {
-    super.onResume();
-  }
-
-  @Override
-  public void onPause() {
-    super.onPause();
-  }
-
-  @Override
-  public void onDestroy() {
-    super.onDestroy();
+    statusTextView = (TextView)findViewById(R.id.status);
   }
 
   public String todaysDate() {
-      DateFormat dateFormat = new SimpleDateFormat("MMMMM d");
+      DateFormat dateFormat = new SimpleDateFormat("MMMM d");
       Date date = new Date();
       return dateFormat.format(date);
   }
